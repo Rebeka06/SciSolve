e7d636816b17bee635a53b781cb02c86777f4b3e
Rebeka06 <rebekanersesyan.06@gmail.com>
2026-05-07 03:01:28 +0400

    Delete FeatureItem.cs

--- FeatureItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SciSolve
-{
-    public class FeatureItem
-    {
-        public string Icon { get; set; } = "";
-        public string Title { get; set; } = "";
-        public string Desc { get; set; } = "";
-    }
-}
